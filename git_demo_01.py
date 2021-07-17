dcc42da31edd2b7cc85c1bd0024e7366da3359ff
Devinesi <suncheng45@163.com>
2021-07-17 19:33:01 +0800

    创建git_demo_01.py文件

---
+++ git_demo_01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time   :2021/7/17 19:04
+# @Author :Kevin
+# @File   :git_demo_01.py
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+driver = WebDriver()
+driver.get("https://www.cnblogs.com/yoyoketang/p/6123834.html")
+driver.implicitly_wait(3)
+driver.quit()
